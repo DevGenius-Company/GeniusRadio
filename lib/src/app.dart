cf23d4183cc688bc839cbad9c6dec68c0c224ed8
Francesco La Forgia <tecfra98@gmail.com>
2021-03-04 11:32:09 +0100

    First push for ui

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:genius_radio/resources/app_config.dart';
 
 void mainCommon({@required String host}) {
@@ -42,12 +43,60 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return NeumorphicTheme(
+      theme: NeumorphicThemeData(
+        baseColor: Color.fromRGBO(248,247,247,1),
+        shadowLightColor: Colors.white,
+        accentColor: Colors.cyan,
+        depth: 6,
+        intensity: 0.5,
       ),
-      body: Center(
-        child: Text(AppConfig.of(context).host),
+      child: Scaffold(
+        backgroundColor: Color.fromRGBO(248,247,247,1),
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  NeumorphicButton(
+                      padding: EdgeInsets.all(0),
+                      style: NeumorphicStyle(
+                        shadowLightColor: Colors.white,
+                        surfaceIntensity: 0.15,
+                        boxShape: NeumorphicBoxShape.circle(),
+                        shape: NeumorphicShape.flat,
+                      ),
+                      child: CircleAvatar(radius:40,backgroundImage: Image.network("https://avatars.githubusercontent.com/u/80033394?s=400&u=c7ab6ed6420f6a3bfd7a06b77823e6330a4de7f4&v=4").image,),
+                      onPressed: () {}),
+                  Spacer(),
+                  NeumorphicButton(
+                      style: NeumorphicStyle(
+                        surfaceIntensity: 0.15,
+                        boxShape: NeumorphicBoxShape.circle(),
+                        shadowLightColor: Colors.white,
+                        shape: NeumorphicShape.flat,
+                      ),
+                      child: Icon(Icons.share_outlined,size: 40,color: Color.fromRGBO(26,41,75,1),),
+                      onPressed: () {}),
+                ],
+              ),
+            ),
+            Neumorphic(
+                padding: EdgeInsets.all(10),
+                style: NeumorphicStyle(
+                    boxShape: NeumorphicBoxShape.circle(),
+                    depth: 8,
+                    lightSource: LightSource.topLeft,
+                    color: Colors.white
+                ),
+                child: CircleAvatar(radius:200,backgroundImage: Image.network("https://avatars.githubusercontent.com/u/80033394?s=400&u=c7ab6ed6420f6a3bfd7a06b77823e6330a4de7f4&v=4").image,)
+            ),
+          ],
+        ),
       ),
     );
   }
